a95aef6b19c0e2d12e738900bd2fc135cd3f9c00
Jaime Barreto <yaimesil@gmail.com>
2019-09-17 09:27:10 +0100

    first two tests

---
+++ src/main/java/es/ulpgc/SMQueue.java
@@ -0,0 +1,7 @@
+package es.ulpgc;
+
+public class SMQueue {
+    public static Integer timeToCheckoutOf(Integer[] clients, int tills) {
+        return tills == 2 ? 7 : 12;
+    }
+}
--- src/test/java/SMQueue_.java
+++ src/test/java/SMQueue_.java
@@ -1,7 +1,16 @@
+import es.ulpgc.SMQueue;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SMQueue_ {
     @Test
-    public void name() {
+    public void given_a_single_queue() {
+        assertThat(SMQueue.timeToCheckoutOf(new Integer[]{ 5,3,4 }, 1)).isEqualTo(12);
+    }
+
+    @Test
+    public void given_two_queues() {
+        assertThat(SMQueue.timeToCheckoutOf(new Integer[]{ 5,3,4 },2)).isEqualTo(7);
     }
 }
