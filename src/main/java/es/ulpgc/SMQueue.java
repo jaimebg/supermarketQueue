18700fa9395e0324428d33efe51036830fd9269f
Jaime Barreto <yaimesil@gmail.com>
2019-09-17 09:34:42 +0100

    third case test

--- src/main/java/es/ulpgc/SMQueue.java
+++ src/main/java/es/ulpgc/SMQueue.java
@@ -2,6 +2,8 @@ package es.ulpgc;
 
 public class SMQueue {
     public static Integer timeToCheckoutOf(Integer[] clients, int tills) {
-        return tills == 2 ? 7 : 12;
+        Integer result = 0;
+        for ( Integer time : clients ) result += time;
+        return tills == 2 ? 7 : result;
     }
 }
--- src/test/java/SMQueue_.java
+++ src/test/java/SMQueue_.java
@@ -5,12 +5,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SMQueue_ {
     @Test
-    public void given_a_single_queue() {
+    public void given_a_single_queue_return_the_sum_of_clients() {
         assertThat(SMQueue.timeToCheckoutOf(new Integer[]{ 5,3,4 }, 1)).isEqualTo(12);
+        assertThat(SMQueue.timeToCheckoutOf(new Integer[]{ 5,9,4 }, 1)).isEqualTo(18);
     }
 
     @Test
-    public void given_two_queues() {
+    public void given_two_queues_return_the_sum_of_queue_with_more_clients_sum() {
         assertThat(SMQueue.timeToCheckoutOf(new Integer[]{ 5,3,4 },2)).isEqualTo(7);
     }
 }
